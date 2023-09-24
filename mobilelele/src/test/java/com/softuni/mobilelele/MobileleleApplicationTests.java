491a3b532ba58a7620d47c1008f49eb2fdc903c1
NikolaYordanov <nikola_bg94@abv.bg>
2023-09-24 14:43:38 +0300

    Initial Commit

---
+++ mobilelele/src/test/java/com/softuni/mobilelele/MobileleleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.mobilelele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileleleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
